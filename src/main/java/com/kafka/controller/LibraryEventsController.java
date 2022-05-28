5272473c181dbdb23ba9500cf960a8e9f8b45d64
devleomeloo <leomelojesus@gmail.com>
2022-05-28 03:14:00 -0300

    Created synchronous call to producer a msg in the topic

--- src/main/java/com/kafka/controller/LibraryEventsController.java
+++ src/main/java/com/kafka/controller/LibraryEventsController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
 @RestController
 public class LibraryEventsController {
 
@@ -18,9 +21,14 @@ public class LibraryEventsController {
 
     @PostMapping("/libraries/events/v1")
     public ResponseEntity<LibraryEvent> postLibraryEvent(
-            @RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+            @RequestBody LibraryEvent libraryEvent)
+            throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
 
-        libraryEventProducer.sendLibraryEvent(libraryEvent);
+/*      Chamada assíncrona para configurar o Kafka Template com mensagem de sucesso publica no topico
+        libraryEventProducer.sendLibraryEvent(libraryEvent);*/
+
+        // Chamada síncrona para configurar o Kafka Template sem a necessidade de mensagem de validação
+        libraryEventProducer.sendLibraryEventSynchronous(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
--- src/main/java/com/kafka/producer/LibraryEventProducer.java
+++ src/main/java/com/kafka/producer/LibraryEventProducer.java
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 @Component
 @Slf4j
 public class LibraryEventProducer {
@@ -38,6 +42,27 @@ public class LibraryEventProducer {
                 handleSuccess(key, value, result);
             }
         });
+    }
+
+    public SendResult<Integer, String> sendLibraryEventSynchronous(LibraryEvent libraryEvent)
+            throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+
+        Integer key = libraryEvent.getLibraryEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+        SendResult<Integer, String> sendResult = null;
+
+        try {
+            sendResult = kafkaTemplate.sendDefault(key, value).get(1, TimeUnit.SECONDS);
+        } catch (ExecutionException | InterruptedException ex) {
+            log.info("ExecutionException/InterruptedException Sending the Message and the exception is: {}",
+                    ex.getMessage());
+            throw ex;
+        } catch (Exception ex) {
+            log.info("Exception Sending the Message and the exception is: {}",
+                    ex.getMessage());
+            throw ex;
+        }
+        return sendResult;
     }
 
     private void handleFailure(Integer key, String value, Throwable ex){
