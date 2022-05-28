0d8cba0811f03280f31556a023f8b128614b364f
devleomeloo <leomelojesus@gmail.com>
2022-05-28 02:22:24 -0300

    Created LibraryEventProcucer class and configured in the Controller

--- src/main/java/com/kafka/controller/LibraryEventsController.java
+++ src/main/java/com/kafka/controller/LibraryEventsController.java
@@ -1,6 +1,9 @@
 package com.kafka.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kafka.domain.LibraryEvent;
+import com.kafka.producer.LibraryEventProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LibraryEventsController {
 
+    @Autowired
+    LibraryEventProducer libraryEventProducer;
+
     @PostMapping("/libraries/events/v1")
     public ResponseEntity<LibraryEvent> postLibraryEvent(
-            @RequestBody LibraryEvent libraryEvent){
+            @RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+
+        libraryEventProducer.sendLibraryEvent(libraryEvent);
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
