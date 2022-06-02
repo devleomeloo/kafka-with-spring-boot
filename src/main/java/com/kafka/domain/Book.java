39dd7e4744bd088c909ac6acf5330c5e76f2028e
devleomeloo <leomelojesus@gmail.com>
2022-06-02 00:58:22 -0300

    Added validation to the book fields

--- src/main/java/com/kafka/domain/Book.java
+++ src/main/java/com/kafka/domain/Book.java
@@ -5,13 +5,21 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class Book {
 
+    @NotNull(message = "Book Id can't be null !")
     private Integer bookId;
+
+    @NotBlank(message = "Book Name can't be blank !")
     private String bookName;
+
+    @NotBlank(message = "Book Author can't be blank !")
     private String bookAuthor;
 }
